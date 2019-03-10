2ff0b94e6f5f5257a4643e2ee4f866757b928d5a
System Administrator <root@Brainiac-2.local>
2019-03-09 23:11:02 -0300

    MyPlayground - uso de arrays, func e variáveis

--- eggplant-brownie/MyPlayground.playground/Contents.swift
+++ eggplant-brownie/MyPlayground.playground/Contents.swift
@@ -22,3 +22,50 @@ func add(name:String, calories:Double){
 
 
 add(name: "Lasagna", calories: 50.5)
+
+
+// array
+let totalCalories = [50.5, 100, 300, 500, 450]
+print(totalCalories)
+
+
+//laços
+
+for i in 0...3{
+    print(i)
+    print("calories are  \(totalCalories[i]) ")
+}
+
+for i in 0...(totalCalories.count - 1){
+    print("calories are  \(totalCalories[i]) ")
+}
+
+for calories in totalCalories {
+    print(calories)
+}
+
+
+//let empty:Array<Int> = []
+
+func allCalories(calories:Array<Double>) -> Double{
+    var total:Double = 0
+    for c in calories {
+        total += c
+    }
+    
+    print("Total: \(total)")
+    return total
+}
+
+allCalories(calories: [50.5, 100, 300, 500, 450])
+
+
+var notas:Array<Double> = [10, 10, 10, 10]
+var soma:Double = 0
+for nota in notas {
+    soma += nota
+}
+
+//print("Média do aluno \(soma / notas.count)")
+
+print("Média do aluno \(Double(soma) / Double(notas.count))")

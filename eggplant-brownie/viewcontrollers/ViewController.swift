e59067634b14275d1127a09cf1e23bc26ec10bef
System Administrator <root@Brainiac-2.local>
2019-03-10 14:23:16 -0300

    Atualizações e validações

--- eggplant-brownie/viewcontrollers/ViewController.swift
+++ eggplant-brownie/viewcontrollers/ViewController.swift
@@ -10,13 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var nameField : UITextField!;
-    @IBOutlet var happinessField : UITextField!;
+    @IBOutlet var nameField : UITextField?
+    @IBOutlet var happinessField : UITextField?
 
     @IBAction func add(){
-        let name = nameField.text;
-        let happiness = happinessField.text;
-        print("eaten \(name) with happiness \(happiness)!");
+        if(nameField == nil || happinessField == nil){
+           return
+        }
+    
+        let name:String = nameField!.text!;
+    
+        if  let happiness = Int(happinessField!.text!) {
+            let meal = Meal(name: name, happiness: happiness)
+            print("eaten \(meal.name) with happiness \(meal.happiness)!");
+        }
     }
 }
 

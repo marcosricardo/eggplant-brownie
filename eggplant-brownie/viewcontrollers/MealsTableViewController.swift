6adda3c11520e01c3e1626da3ca74389996af583
System Administrator <root@Brainiac-2.local>
2019-03-12 23:44:00 -0300

    func para adicionar refeições - compartilhando objetos entre controllers

--- eggplant-brownie/viewcontrollers/MealsTableViewController.swift
+++ eggplant-brownie/viewcontrollers/MealsTableViewController.swift
@@ -11,8 +11,23 @@ import UIKit
 class MealsTableViewController : UITableViewController{
     
     //array de Meals
-    var meals = [Meal(name: "Lasagna", happiness: 5), Meal(name: "Spaghetti", happiness: 5), Meal(name: "Pizza", happiness: 5)]
+    //var meals = [Meal(name: "Lasagna", happiness: 5), Meal(name: "Spaghetti", happiness: 5), Meal(name: "Pizza", happiness: 5)]
+    var meals:Array<Meal> = []
+    
+    //adiciona item passado por parâmetro no array meals 
+    func add(meal:Meal) {
+        meals.append(meal)
+        tableView.reloadData()
+    }
    
+    //Este método é executado pouco antes da troca de Views, quando a mesma é solicitada pelo usuário.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let view = segue.destination as! ViewController
+        //preenche a propriedade mealsTable da classe ViewController com a classe MealsTableViewController (em questão)
+        view.mealsTable = self
+    }
+    
+    
     //define quantas linhas terá a tabela
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return meals.count
--- eggplant-brownie/viewcontrollers/ViewController.swift
+++ eggplant-brownie/viewcontrollers/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
     
     @IBOutlet var nameField : UITextField?
     @IBOutlet var happinessField : UITextField?
+    //instância objeto do tipo MealsTableViewController
+    var mealsTable : MealsTableViewController?
 
+  
     @IBAction func add(){
         if(nameField == nil || happinessField == nil){
            return
@@ -23,6 +26,9 @@ class ViewController: UIViewController {
         if  let happiness = Int(happinessField!.text!) {
             let meal = Meal(name: name, happiness: happiness)
             print("eaten \(meal.name) with happiness \(meal.happiness)!");
+            
+            //adiciona meal acessando func add da classe MealsTableViewController
+            mealsTable!.add(meal: meal)
             
             /* navigationController é opcional "navigationController?.popViewController(animated: true)", isto evita crash validando a variável navigationController
             */

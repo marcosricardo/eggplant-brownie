556ac38ab73a6f9e44ed076f2834846bc53f4af6
Marcos Ricardo <mricardoalves99@gmail.com>
2019-03-24 03:16:51 -0300

    Seleçao de itens da refeiçao

--- eggplant-brownie/viewcontrollers/ViewController.swift
+++ eggplant-brownie/viewcontrollers/ViewController.swift
@@ -8,14 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var nameField : UITextField?
     @IBOutlet var happinessField : UITextField?
     //instância objeto do tipo MealsTableViewController
     var delegate : AddAMealDelegate?
 
+    var items:Array<Item> = [
+        Item(name: "Queijo", calories: 22.5),
+        Item(name: "Molho de tomate", calories: 22.5),
+        Item(name: "Carne moída", calories: 22.5),
+    ]
+    
+    var selected : Array<Item> = []
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath){
+            //caso não o item não esteja selecionado ele é marcado com o ícone checkmark
+            if(cell.accessoryType == UITableViewCell.AccessoryType.none) {
+                cell.accessoryType = UITableViewCell.AccessoryType.checkmark
+                let item = items[indexPath.row]
+                //adiciona item selecionado
+                selected.append(item)
+            } else {
+                 //retira ícone checkmark caso esteja selecionado no clique do usuário
+                cell.accessoryType = UITableViewCell.AccessoryType.none
+                
+                let item = items[indexPath.row]
+                let position = selected.index(of: item)
+                //remove item da posição encontrado no array selected
+                selected.remove(at: position!)
+            }
+        }
+    }
   
+    //define quantas linhas terá a tabela
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    //retorna o conteúdo de cada linha (célula) da tabela
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        let item = items[row]
+        
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: nil)
+        cell.textLabel!.text = item.name //adicionamos o '.name' para exibir apenas o nome do item da refeição na tabela
+        return cell
+    }
+    
+    
     @IBAction func add(){
         if(nameField == nil || happinessField == nil){
            return
@@ -24,8 +67,8 @@ class ViewController: UIViewController {
         let name:String = nameField!.text!;
     
         if  let happiness = Int(happinessField!.text!) {
-            let meal = Meal(name: name, happiness: happiness)
-            print("eaten \(meal.name) with happiness \(meal.happiness)!");
+            let meal = Meal(name: name, happiness: happiness, items: selected)
+            print("eaten \(meal.name) with happiness \(meal.happiness)! and itens: \(meal.items)");
             
             //adiciona meal acessando func add da classe MealsTableViewController
             delegate!.add(meal)
